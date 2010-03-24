c091720e3fcae91bb1bd25b7519235f1de6274ac
Gustaf Sjöberg <gs@distrop.com>
2010-03-24 19:38:13 +0100

    Added a supervisor.

---
+++ src/mimir_sup.erl
@@ -0,0 +1,14 @@
+-module(mimir_sup).
+-behaviour(supervisor).
+-export([start_link/0]).
+-export([init/1]).
+ 
+start_link() ->
+    supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+ 
+init([]) ->
+    Processes = [
+        {cache, {cache, start, []}, permanent, brutal_kill, worker, [cache]},
+        {mimir, {mimir, start, []}, permanent, brutal_kill, worker, dynamic}
+    ],
+    {ok, {{one_for_one, 5, 60}, Processes}}.
